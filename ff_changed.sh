b29a131ba34bcebc5349e4d585c7b1497b4d1b31
Christian Werner <christian.werner@the-wogo.com>
2012-06-17 22:10:03 +0200

    init

---
+++ ff_changed.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+usage() {
+	echo ##Search whole filesystem for changed files in the last $1 minutes
+}
+find /bin /dev /etc /home /lib /media /mnt /opt /root /sbin /srv /tmp /usr /var -cmin -$1
